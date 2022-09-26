b2a62cbf3fb775aad3b5620bf63b42d268f1d402
frustrabe <lukas.forner@gmail.com>
2022-09-26 14:01:35 +0200

    added structure and object lesson

---
+++ data-structures/objects.js
@@ -0,0 +1,34 @@
+/* OBJECTS - JS type
+           - Data Structure in JS
+They consist of a property-value pairs and are 
+assigned to a variable.
+
+In arrays we used the [number] to acces a specific 
+properties and their values stored in the array. With objects 
+we use "variable.property"
+Example using the code below:
+*/
+
+const userObject = {
+  name: "John",
+  age: "28",
+  hobby: "code",
+  isMarried: false,
+};
+
+/* 
+userObject.name;
+'John'
+userObject.isMarried;
+false
+ */
+
+/* 
+Adding a property to an object or re-writing 
+an existing value:
+
+userObject.favouriteFood = "vegetables"
+
+userObject.isMarried = true
+
+ */
