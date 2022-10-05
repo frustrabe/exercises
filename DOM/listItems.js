718f3e7ce8e5fa9b7d0d0655271dbc940ea60357
frustrabe <lukas.forner@gmail.com>
2022-10-05 21:42:28 +0200

    further progress with eventListener4

--- DOM/listItems.js
+++ DOM/listItems.js
@@ -1,12 +1,17 @@
 // Defining our main variables from html file
-const button = document.getElementById("add-button");
+const addButton = document.getElementById("add-button");
 const input = document.getElementById("user-input");
 const ul = document.querySelector("ul");
+const resetButton = document.getElementById("reset-button");
+
+function inputLength() {
+  return input.value.length;
+}
 
 // Adding and eventListener "click" to our button
-button.addEventListener("click", function () {
+addButton.addEventListener("click", function () {
   // Condition to prevent submition of empty fields
-  if (input.value.length > 0) {
+  if (inputLength() > 0) {
     // variable "list" gets created
     let li = document.createElement("li");
     // variable "textNode" gets created and grabs the value
@@ -25,7 +30,7 @@ button.addEventListener("click", function () {
 input.addEventListener("keypress", function (event) {
   // Condition to prevent submition of empty fields and
   // adding "Enter" as a form of submition
-  if (input.value.length > 0 && event.code === "Enter") {
+  if (inputLength() > 0 && event.code === "Enter") {
     // variable "list" gets created
     let li = document.createElement("li");
     // variable "textNode" gets created and grabs the value
@@ -38,4 +43,9 @@ input.addEventListener("keypress", function (event) {
     // input field value gets cleared to an empty string
     input.value = "";
   }
+});
+
+// Trying to add a reset button
+resetButton.addEventListener("click", function () {
+  input.value = "";
 });
